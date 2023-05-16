d01be2431895f1c73c9aec3cd26e8e000ed2136d
imgaurav30 <gauravgupta2240@gmail.com>
2023-05-16 18:36:19 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Form from './components/Form';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-10/12 mx-auto">
+      <h1 className='text-4xl font-bold mt-3 border-b-4 border-green-900 inline-block py-2 px-4'>Form - Project</h1>
+      <div>
+      <Form/>
+      </div>
     </div>
   );
 }
